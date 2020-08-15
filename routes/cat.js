39b9abde6f4e138dd00a1f02bb77e50e16d6fe5e
unknown <supriyo.developer@gmail.com>
2020-08-15 15:37:53 +0530

    🐱 router

---
+++ routes/cat.js
@@ -0,0 +1,16 @@
+// Import packages
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+router.get('/', async (req, res) => {
+    // Fetching data from thecatapi
+    const _res = await fetch('https://api.thecatapi.com/v1/images/search')
+	const data = await _res.json();
+
+        res.render('cat.ejs', {
+            imgURL: data[0].url // Image URL
+        })
+})
+
+module.exports = router;
