9d4e8e9de38ca66338b39fea4e5b74934f475bfe
Riday <ridayshah2001@gmail.com>
2020-10-01 09:36:50 +0530

    Refactor: cleaned up code

--- src/routes/dog.js
+++ src/routes/dog.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
-router.get('/', async (req, res) => {
+router.get('/', async (_req, res) => {
 	// Fetching data from dog.ceo
 	const data = await fetch('https://dog.ceo/api/breeds/image/random').then(res => res.json());
 	const breedName = data.message.split('/');
-	const name = breedName[4].charAt(0).toUpperCase() + breedName[4].slice(1);
+	const name = breedName[4].replace(/\b(\w)/g, char => char.toUpperCase());
 
 	res.render('dog.ejs', {
 		imgURL: data.message,  // Image URL
